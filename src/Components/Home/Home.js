d808db1ec46505c7ba123bf8aa35c8505c766d46
Raju <ashfaqraju27@gmail.com>
2022-10-18 01:55:23 +0600

    home page design complete

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
-import './Home.css'
+import QuizTopic from '../QuizTopic/QuizTopic';
+//import './Home.css'
 
 const Home = () => {
-    const quizTopics = useLoaderData([]);
+    const topics = useLoaderData({});
     
     return (
         <div>
-            <h3 className='home'>Welcome to the educational web-site. </h3>
+            <h3 className='home mt-10 text-2xl font-semibold'>Welcome to the educational web-site. {topics.data.length} </h3>
+            <div className='quiz-topic mt-10 flex justify-between'>
+                {
+                    topics.data.map(topic => <QuizTopic
+                    key={topic.id}
+                    topic={topic}></QuizTopic>)
+                }
+            </div>
         </div>
     );
 };
--- src/Components/QuizTopic/QuizTopic.js
+++ src/Components/QuizTopic/QuizTopic.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import { BeakerIcon, ArrowRightIcon } from '@heroicons/react/24/solid'
+//import './QuizTopic.css';
 
-const QuizTopic = () => {
+const QuizTopic = ({topic}) => {
+    const {id, name, logo} = topic;
     return (
-        <div>
+        <div className='m-5 p-2 border-2 border-sky-500 rounded-md'>
+            
+            <img className='border-2 border-emerald-500 rounded-md' src={logo} alt="" />
+            <div className='mt-5 mb-3 text-xl'>
+                
+                <h3>{name} </h3>
+                
+            </div>
+
+            <button className='flex bg-amber-400 w-full rounded-lg p-2'>
+                <div className='text-xl  m-auto w-50'>
+
+                    Quiz
+
+                </div>
+                { <div className='m-1 '>
+                    <ArrowRightIcon className="h-6 w-6 text-blue-500"/>
+                </div> }
+            </button>
             
         </div>
+        
     );
 };
 
