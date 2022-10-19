485a9ba9b3b2328798b83c96cb83758876c67a9c
Raju <ashfaqraju27@gmail.com>
2022-10-19 23:36:40 +0600

    simple change on quiz section

--- src/Components/Quiz/Quiz.js
+++ src/Components/Quiz/Quiz.js
@@ -5,14 +5,14 @@ const Quiz = ({quiz}) => {
   const questionData = quiz;
   return (
     <div>
-      <h1>Bal chal 
+      
         {
           questionData.map(question => <Questions
           key={question.id}
           question={question}>
             
           </Questions>)
-        }</h1>
+        }
     </div>
   );
 };
